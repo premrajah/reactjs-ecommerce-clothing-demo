260acd00215ae30bc7239c6810e7e3dc9db7212c
Prem R <prem.rajah@gmail.com>
2020-07-14 22:40:14 +0100

    update menu items with react router update directory with higher order function react router edited app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import './App.css';
+
+const Hats = () => <h1>HATS</h1>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
