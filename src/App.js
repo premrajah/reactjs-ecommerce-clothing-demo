94147104bcbe42c386c7f3f42cb5bd1c557ec053
Prem R <prem.rajah@gmail.com>
2020-07-14 23:07:11 +0100

    added collection-preview comp added shop page added shop data as external file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
-
-const Hats = () => <h1>HATS</h1>;
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={Hats} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
